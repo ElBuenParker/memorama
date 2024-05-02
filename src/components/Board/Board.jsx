86532623f9b5dff8eb4bdd6ec7c05a7adde35def
ElBuenParker <mr.gonzalex.4@gmail.com>
2024-05-02 14:58:20 -0600

    gh pages added

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,26 @@
+import MemoBlock from '../MemoBlock/MemoBlock';
+import './Board.css';
+
+
+const Board = ({animating, handleMemoClick, memoBlocks}) => {
+    return (
+        <div className="mainScreen">
+            <div className="header">Memorama
+            <button>
+                <span class="box">
+                    Reiniciar
+                </span>
+            </button>
+</div>
+            <div className="board">
+                {memoBlocks.map( (memoBlock, i) => {
+                    return <MemoBlock key={`${i}_${memoBlock.emoji}`} animating={animating} handleMemoClick={handleMemoClick} memoBlock={memoBlock} />
+                })}
+            </div>
+
+        </div>
+        
+    );
+}
+
+export default Board;
